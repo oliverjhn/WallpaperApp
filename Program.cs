412ea7289e6c4f688eebaaef861e1115f4239424
Oliver Hogan <oliver@oliver-hogan.com>
2024-03-25 14:49:05 +1100

    stuff

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.IO;
 using Octokit;
+//This is a library that simplifies access to the command line, and allows easily setting up arguments and verbs
+using CommandLine;
 
 namespace WallpaperApp;
 
 internal class BackgroundChanger
 {
+    //Using CommandLine, this code sets up the verb 'run'
+    [Verb("run", HelpText = "Re-runs the program and sets a new wallpaper. This will replace the current wallpaper.")]
+    public class RunOptions
+    {
+    }
+
+    //Using CommandLine, this code sets up the verb 'interval'
+    [Verb("interval", HelpText = "Sets the interval that the program should run. A value of 0 never re-runs the program")]
+    public class InvervalOptions
+    {
+    }
+
     public const int SPI_SETDESKWALLPAPER = 20;
     public const int SPIF_UPDATEINFILE = 1;
     public const int SPIF_SENDCHANGE = 2;
@@ -15,10 +31,40 @@ internal class BackgroundChanger
 
     private static void Main(string[] args)
     {
-        Task.WaitAny(GetInfo());
+        //Using CommandLine, this code defines what code should run for each argument from the CLI
+        var result = Parser.Default.ParseArguments<RunOptions, InvervalOptions>(args)
+            .WithParsed<RunOptions>(options => WallpaperRun())
+            .WithParsed<InvervalOptions>(options => WallpaperInterval());
+
         Console.WriteLine("Press any key to exit.");
         Console.ReadKey();
     }
+
+    private static void WallpaperRun()
+    {
+        Console.WriteLine("Program execution should start now");
+        //Checks if the directory exists, and if it doesn't, then the program creates it and informs the user
+        if (!Directory.Exists(@"C:\Wallpapers"))
+        {
+            Directory.CreateDirectory(@"C:\Wallpapers");
+            Console.WriteLine("Created directory \"C:\\Wallpapers\"");
+        }
+        //This code gets some of the different image filetypes in the folder, and merges them into one string[] and the end.
+        string[] pngFilePaths = Directory.GetFiles(@"C:\Wallpapers\", "*.png", SearchOption.TopDirectoryOnly);
+        string[] jpgFilePaths = Directory.GetFiles(@"C:\Wallpapers\", "*.jpg", SearchOption.TopDirectoryOnly);
+        string[] webpFilePaths = Directory.GetFiles(@"C:\Wallpapers\", "*.webp", SearchOption.TopDirectoryOnly);
+        string[] imagePaths = pngFilePaths.Concat(jpgFilePaths.Concat(webpFilePaths).ToArray()).ToArray();
+
+        //Randomly chooses an image from the directory and sets it as the wallpaper
+        string imageDirectory = imagePaths[new Random().Next(0, imagePaths.Length)];
+        SetWallpaper(imageDirectory);
+    }
+
+    private static void WallpaperInterval()
+    {
+        Console.WriteLine("Interval code");
+    }
+
 
     private static async Task<string> GetInfo()
     {
@@ -26,12 +72,11 @@ internal class BackgroundChanger
         var gitHubClient = new GitHubClient(new ProductHeaderValue("WallpaperApp"));
         //Asynchronously gets all the contents of a github repo. In this case, I am using a wallpaper repository.
         var repositoryContents = await gitHubClient.Repository.Content.GetAllContents("D3Ext", "aesthetic-wallpapers");
-        Console.WriteLine(repositoryContents);
         //Uses the repo that we got earlier to search the given path
-        RepositoryContent? firstOrDefault = repositoryContents.FirstOrDefault(c => c.Path == @"images/3squares.png");
-        Console.WriteLine(firstOrDefault);
+        RepositoryContent? firstOrDefault = repositoryContents.FirstOrDefault(c => c.Path == @"/images/3squares.png");
         //Gets the raw download url from the file path, and returns it
         string? downloadUrl = firstOrDefault?.DownloadUrl;
+        Console.WriteLine(downloadUrl);
         return downloadUrl;
     }
 
